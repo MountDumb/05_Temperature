56266e0b99d4398da7efe71b51b6f560fdbe7ed7
UrbanSkurk <UrbanSkurk@HELL>
2016-09-15 10:43:01 +0200

    basics done.

---
+++ 05_Temperature/Temperature.cs
@@ -0,0 +1,58 @@
+﻿namespace _05_Temperature
+{
+    // Celcius freezes at 0, fahrenheit at 32.
+    // Fahrenheit is 5/9 of celsius
+    internal enum Unit {Fahrenheit, Celcius }
+       
+
+    internal class Temperature
+    {
+        //Fields
+        private double fahrenheit;
+        private double celcius;
+        
+        //Methods
+        public Temperature(Unit unit, double temp)
+        {
+            if (unit == Unit.Fahrenheit)
+            {
+                fahrenheit = temp;
+            }
+            else if (unit == Unit.Celcius)
+            {
+                celcius = temp;
+            }
+        }
+        public Temperature()
+        { }     
+
+        public static double FahrenheitToCelcius(double temp)
+        {
+            return (temp - 32) * 5 / 9; 
+
+        }
+
+        public static double CelciusToFahrenheit(double temp)
+        {
+            return (temp * 9 / 5) + 32;
+        }
+
+        // Properties
+
+        public double Fahrenheit
+        {
+            get { return fahrenheit; }
+            set { fahrenheit = value; }
+        }
+
+        public double Celcius
+        {
+            get { return celcius; }
+            set { celcius = value; }
+        }
+
+    }
+
+
+}
+
